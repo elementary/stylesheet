cf1813da6f893993470b0453a6124a05c0e2d157
Daniel Fore <daniel.p.fore@gmail.com>
2011-02-01 15:10:52 -0800

    xfce4 window theme

---
+++ xfwm4/inactive-no-buttons.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Use new theme (invisible buttons on unfocused windows)
+cp -f close-inactive-new.png close-inactive.png
+cp -f close-inactive-new.xpm close-inactive.xpm
+cp -f hide-inactive-new.png hide-inactive.png
+cp -f hide-inactive-new.xpm hide-inactive.xpm
+cp -f maximize-inactive-new.png maximize-inactive.png
+cp -f maximize-inactive-new.xpm maximize-inactive.xpm
+cp -f menu-inactive-new.png menu-inactive.png
+cp -f menu-inactive-new.xpm menu-inactive.xpm
+echo "Remember to de-select and then select this theme"
+echo "in XFCE's Window Manager Settings"
